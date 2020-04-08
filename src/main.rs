d48556c0bd9292c2b9172872ec05fb7e38a95e0c
EndCat <endcat749@gmail.com>
2020-04-08 07:18:50 -0700

    minimal kernel succ

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 #![no_std] // no rust stdlib
 #![no_main] // disable rust-level entry point
-use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
-// this function is called on panic
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+#![allow(unused_imports)]
+use nekoos;
