f13b60f1cebfe990dca221f83b3c9cd2f7505794
Yuki Kitajima <admin@endcat.cn>
2021-04-16 11:35:31 +0800

    Update minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // this func is the entry point, since the linker looks for a func named '_start' by default
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
